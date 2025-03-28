2999e28e6b9c5eaa5cd9f1dcb6d3d472fe720344
Marinegrn <marinegarein@outlook.fr>
2025-03-28 09:54:52 +0100

    test API & requête ville Paris

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+//const city = document.getElementById('cityInput');
+
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    // test de la requête pour la ville de Paris & de l'API -> OK
+    // console.log(data); 
+};
+
+fetchData('https://nominatim.openstreetmap.org/search?q=Paris&format=json&addressdetails=1&limit=1');
+
+
