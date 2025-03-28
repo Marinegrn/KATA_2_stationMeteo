5a8f70398ca2d8b2d764f197c941e2860ea8ac6a
Marinegrn <marinegarein@outlook.fr>
2025-03-28 10:09:26 +0100

    test bouton OK

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-//const city = document.getElementById('cityInput');
+const city = document.getElementById('cityInput');
+const okButton = document.getElementById('okButton');
 
-async function fetchData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    // test de la requête des coordonnées de la ville de Paris & Lyon -> OK
-     console.log(data); 
+// test de la requête des coordonnées de la ville de Paris & Lyon -> OK
+// async function fetchData(url) {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//      console.log(data); 
+// };
+
+// fetchData('https://nominatim.openstreetmap.org/search?q=Lyon&format=json&addressdetails=1&limit=1');
+
+async function fetch(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        }   catch (error) {
+        console.error('Error fetching data:', error);
+    };
 };
 
-fetchData('https://nominatim.openstreetmap.org/search?q=Lyon&format=json&addressdetails=1&limit=1');
+okButton.addEventListener('click', () => {
+    const cityName = city.value;
+    fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(cityName)}&format=json&addressdetails=1&limit=1`);
+});
+
+
 
 
